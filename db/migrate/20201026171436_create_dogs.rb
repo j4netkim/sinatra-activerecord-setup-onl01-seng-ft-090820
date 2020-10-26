93158e173876db8ac5bd33d8f2d0312dd0a0e4b8
J K <jkim65@buffalo.edu>
2020-10-26 17:18:31 +0000

    Automatically backed up by Learn

--- db/migrate/20201026171436_create_dogs.rb
+++ db/migrate/20201026171436_create_dogs.rb
@@ -1,7 +1,12 @@
 class CreateDogs < ActiveRecord::Migration[6.0]
-  def change
+  def up
     create_table :dogs do |t|
-    t.string :name
-    t.string :breed
+      t.string :name
+      t.string :breed
+    end
+  end
+
+  def down
+    drop_table :dogs
   end
 end
